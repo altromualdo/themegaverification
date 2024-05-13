39f5dbe530bd5ad0ebe58db7df18564ff89e6f28
altromualdo <altromualdo@gmail.com>
2024-05-13 13:30:29 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    role_name = "member"
+    message_id = "1239364978633605200"
+    prefix = "."
+    bot_token = "MTIzOTIxNjA2Njg1NzY2ODYzMA.Gnrsu2.wA3Ido4f4x0j0UTzxBvt5cmx269d2MXBYwq9w0"
+    channel_id = "1239215987287658557"
